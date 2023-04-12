0bf9a540d1960f4befbb196752196d4a76724713
Mihnea <stef1eds@gmail.com>
2023-04-12 20:43:11 +0300

    Post functionality

--- link_easy_app/main.py
+++ link_easy_app/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from validators import url
+
+from . import schemas
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return "This is the root of the app! Nothing to see here"
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not url(url.target_url):
+        raise_bad_request("Your provided URL is not valid!")
+    return f"TODO: Create database entry for: {url.target_url}"
