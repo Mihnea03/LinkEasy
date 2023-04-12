f9c6f881631a2a0bfa26aa874d30560c6e2fe301
Mihnea <stef1eds@gmail.com>
2023-04-12 21:12:10 +0300

    Added post functionality

--- link_easy_app/main.py
+++ link_easy_app/main.py
@@ -1,15 +1,41 @@
-from fastapi import FastAPI, HTTPException
-from validators import url
+from fastapi import Depends, FastAPI, HTTPException
+import validators
+import string
+from random import choices
+from sqlalchemy.orm import Session
 
-from . import schemas
+from . import schemas, models
+from .database import engine, SessionLocal
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
-@app.post("/url")
-def create_url(url: schemas.URLBase):
-    if not url(url.target_url):
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    if not validators.url(url.target_url):
         raise_bad_request("Your provided URL is not valid!")
-    return f"TODO: Create database entry for: {url.target_url}"
+    
+    key = "".join(choices(string.ascii_uppercase, k=5))
+    secret_key = "".join(choices(string.ascii_uppercase, k=8))
+    db_url = models.URL(
+        target_url=url.target_url, key=key, secret_key=secret_key
+    )
+
+    db.add(db_url)
+    db.commit()
+
+    db.refresh(db_url)
+    db_url.url = key
+    db_url.admin_url = secret_key
+
+    return db_url
